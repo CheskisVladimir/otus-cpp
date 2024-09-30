fc9f4c47cb3600d6ae57d5b76e9788c270c39344
Vladimir Cheskis <vladimir.talmid@gmail.com>
2024-09-30 21:58:45 +0300

    Change order of last tasks output

--- ip_lib_main.cpp
+++ ip_lib_main.cpp
@@ -55,6 +55,7 @@ namespace ip_lib
         try
         {
             std::vector<ip_t> ip_pool = load();
+            ip_lib::sort_ip_vector(ip_pool, sort_order_t::inverted);
 
             auto cekck_all = [](const ip_t &)
             {
@@ -69,9 +70,7 @@ namespace ip_lib
                     [](const ip_t &ip)
                     { print(ip); });
 
-                std::vector<ip_t> ip_pool_to_sort = ip_pool;
-                ip_lib::sort_ip_vector(ip_pool_to_sort, sort_order_t::inverted);
-                process(ip_pool_to_sort, proc);
+                process(ip_pool, proc);
             }
 
             ip_proc_t proc_byte_1(
