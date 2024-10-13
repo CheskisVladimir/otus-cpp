0792f54b74729baa191dcc70ba8a22908371e6d5
Vladimir Cheskis <vladimir.talmid@gmail.com>
2024-10-13 16:34:50 +0300

    Remove ip_filter from main

--- main.cpp
+++ main.cpp
@@ -2,14 +2,8 @@
 
 #include <iostream>
 
-namespace ip_lib
-{
-int ip_main();
-}
-
 int main(int, char **) {
 	//std::cout << "Version: " << version() << std::endl;
-	//std::cout << "Hello, world!" << std::endl;
-
-	return ip_lib::ip_main();
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
 }
