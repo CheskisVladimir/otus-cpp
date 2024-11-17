595f88f28e98aec8e3fbab378bb7a1d97679309d
Vladimir Cheskis <vladimir.talmid@gmail.com>
2024-11-17 18:04:00 +0300

    MyAppFolder

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
+/*! \file main.cpp
+    \brief It's a main c++ file.
+    
+    Details.
+*/
+
+
+
 #include "lib.h"
 
 #include <iostream>
+
+/**
+ * @brief Calculates sum of the integer numbers
+ *
+ * @param a The  1st argument
+ * @param b The  2nd argument
+ *
+ * @return Sum
+ */
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 
 int main(int, char **) {
 	std::cout << "Version: " << version() << std::endl;
